112b7b5ac5e9691bb77effdf4aa24a2f4556c4cf
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2021-02-21 21:19:48 +0100

    feat(server): add GetApiRequestHandler module

---
+++ src/server/request-handler/GetApiRequestHandler.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import { getServerInfoInteractor } from '../interactors/GetServerInfoInteractor';
+import { ServerInfoApi } from '../models/api/ServerInfoApi';
+import { ServerInfo } from '../models/domain/ServerInfo';
+import { serverInfoToServerInfoApiTransformer } from '../transformer/ServerInfoToServerInfoApiTransformer';
+
+export class GetApiRequestHandler {
+  public handler(request: express.Request, response: express.Response): void {
+    const serverInfo: ServerInfo = getServerInfoInteractor.interact();
+
+    const serverInfoApi: ServerInfoApi = serverInfoToServerInfoApiTransformer.transform(
+      serverInfo,
+    );
+
+    response.send(serverInfoApi);
+    response.status(200);
+  }
+}
