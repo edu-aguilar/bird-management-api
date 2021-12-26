757efa56f4c5b8c3ece17beee434182cb207891d
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2021-12-26 16:10:49 +0100

    ✅ (bird): update breaking fixture

--- src/bird/fixtures/domain/BirdCreationQueryFixtures.ts
+++ src/bird/fixtures/domain/BirdCreationQueryFixtures.ts
@@ -23,10 +23,7 @@ export class BirdCreationQueryFixtures {
       ...this.withMandatory,
       comments: 'some-comments',
       description: 'some-bird-description',
-      images: [
-        'https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-        'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80',
-      ],
+      images: ['https://some-url.com/some-path/some-image.jpg'],
       name: 'bird-name',
       parentsId: [
         'ccc1648a-7b1c-4fda-9af9-6ee1345fb663',
@@ -43,7 +40,7 @@ export class BirdCreationQueryFixtures {
   public static get withImage(): BirdCreationQuery {
     const fixture: BirdCreationQuery = {
       ...this.withMandatory,
-      images: ['https://domain.com/some-image.jpg'],
+      images: ['https://some-url.com/some-path/some-image.jpg'],
     };
 
     return fixture;
@@ -52,7 +49,7 @@ export class BirdCreationQueryFixtures {
   public static get withImageAndRingId(): BirdCreationQuery {
     const fixture: BirdCreationQuery = {
       ...this.withMandatory,
-      images: ['https://domain.com/some-image.jpg'],
+      images: ['https://some-url.com/some-path/some-image.jpg'],
       ringId: 'BIRD-1234',
     };
 
